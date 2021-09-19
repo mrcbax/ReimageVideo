d5e391b75329f91ac3c31534eeeea96395ea9c1d
Chad Baxter <cbax@doslabelectronics.com>
2021-09-19 18:31:57 -0400

    brightness adjust

--- src/display.c
+++ src/display.c
@@ -22,13 +22,13 @@ void draw_group(char group_start, char * text, fill_properties_t * screen_fill) 
       for (char group = group_start; group < 16; group+=2) { //iluminate the groups
         if (screen_fill -> color == 2) {
           BDC_ADDR = get_column(group, segment, 0, column);
-          delay_nop(3);
+          delay_nop(5);
           BDC_ADDR = get_column(group, segment, 1, column);
-          delay_nop(27);
+          delay_nop(40);
         } else {
           //Look up the correct color for the column we want, then enable that column
           BDC_ADDR = get_column(group, segment, screen_fill -> color, column);
-          delay_nop(30); //delay before disabling the column (brightness)
+          delay_nop(45); //delay before disabling the column (brightness)
         }
 
         //delay_ms(500); //<<<<USE THIS DELAY FOR TESTING
